20d42fcee4b2350c17e6a7008e8f444cacd58ff3
dheeraj.jha <dheeraj.jha@phonepe.com>
2020-10-07 12:20:22 +0530

    remove object graph memory lock

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,6 @@ LDIR =../lib
 
 LIBS=-lm
 
-_DEPS = bigINT.h
-DEPS = $(patsubst %,$(IDIR)/%,$(_DEPS))
-
-_OBJ = bigINT.o runner.o
-OBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))
-
-
 $(ODIR)/%.o: %.c $(DEPS)
     $(CC) -c -o $@ $< $(CFLAGS)
 
